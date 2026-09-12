a09a0a28db71dd42c165b9a1c9dfa60f84bdbc36
EvansBarngetuny <evanskibitok006@gmail.com>
2026-09-12 20:11:24 +0300

    Forcing HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\Memo;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Schema;
 
