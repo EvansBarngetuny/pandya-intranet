fa2b66ba8c639227911d031852825452e40e4a5d
EvansBarngetuny <evanskibitok006@gmail.com>
2026-09-13 07:48:58 +0300

    Bootsrap changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,11 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         // Trust to railway's reverse proxy
         $middleware->trustProxies(at: '*');
-
-        $middleware->web(append: [
-            \App\Http\Middleware\HandleInertiaRequests::class,
-            //\Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
-        ]);
+       
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
